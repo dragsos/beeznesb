538ac2cb1a706525aa6c770b237efaacf69a5e9d
dragsos <dragos.sargu@gmail.com>
2023-10-26 04:36:19 +0200

    Robot pathing and updated models/textures

---
+++ Assets/Scripts/RobotPathing (old).cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnWaypoints : MonoBehaviour
+{
+    public List<GameObject> waypoints;
+    public float speed = 2;
+    int index = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 destination = waypoints[index].transform.position;
+        Vector3 newPos = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+        transform.position = newPos;
+
+        float distance = Vector3.Distance(transform.position, destination);
+        if (distance <= 0.05)
+        {
+            index++;
+        }
+    }
+}
