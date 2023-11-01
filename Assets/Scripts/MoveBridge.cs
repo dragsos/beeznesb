b356e76711056ca49527517b6498f1814666b3b2
dragsos <dragos.sargu@gmail.com>
2023-11-01 01:28:52 +0100

    Lever puzzle and updated map

---
+++ Assets/Scripts/MoveBridge.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBridge : MonoBehaviour
+{
+    public float distanceToMove = 2f;
+    public float speed = 3f;
+    public Vector3 finalPosition;
+    public LeverBehavior lever;
+
+    void Start()
+    {
+        finalPosition = transform.position + Vector3.up * distanceToMove;
+    }
+
+    void Update()
+    {
+        if (lever.isOn)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, finalPosition, speed * Time.deltaTime);
+        }
+    }
+}
