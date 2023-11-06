f976c6a08424d4fdbf490369963bb38f55eca719
dragsos <dragos.sargu@gmail.com>
2023-11-06 23:54:13 +0100

    Added rubble and other assets

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
-{
-
+{ 
     public OpenLid openLidScript;
 
     // Start is called before the first frame update
